3c0ca8560cce6a4cee0e249e61828036eab98402
Mark Koo <koocool88@gmail.com>
2021-07-25 20:12:51 +0800

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
+    screens: {
+      pa: "360px", // phone android
+      pi: "414px", // phone iphone
+      pi11: "428px", // phone iphone 11 Pro Max
+      tp: "768px", // tablet portrait
+      ti8p: "810px", // tablet iPad gen 8th portrait
+      tl: "1024px", // tablet landscape
+      ti8l: "1080px", // tablet iPad gen 8th landscape
+      l150: "1280px", // laptop 1920 / 150%
+      // lc: "1366px", // laptop cheap
+      // l125: "1536px", // laptop 1920 / 125%
+      // l100: "1920px", // laptop 1920
     },
+    extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
